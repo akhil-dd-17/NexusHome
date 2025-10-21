efa3570ccb6b9350e26412984da478756bbd02a7
Akhil Dakinedi <akhil.dakinedi@doordash.com>
2025-10-21 16:00:52 -0700

    Initial Commit

---
+++ NexusHome/NexusHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexusHomeApp.swift
+//  NexusHome
+//
+//  Created by Akhil Dakinedi on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct NexusHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NexusHomeTests/NexusHomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  NexusHomeTests.swift
+//  NexusHomeTests
+//
+//  Created by Akhil Dakinedi on 10/21/25.
+//
+
+import Testing
+@testable import NexusHome
+
+struct NexusHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
